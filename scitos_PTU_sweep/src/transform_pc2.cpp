afb925d2fefdc9991db6d3d5c91ec4ec20914503
Jaime Pulido Fentanes <jaime@Hewley.(none)>
2013-09-26 15:32:38 +0100

    Trying to add a tf::message_filter to prevent data loss if no transform is available.

--- scitos_PTU_sweep/src/transform_pc2.cpp
+++ scitos_PTU_sweep/src/transform_pc2.cpp
@@ -2,6 +2,8 @@
 #include <ros/time.h>
 #include "sensor_msgs/PointCloud2.h"
 #include <tf/transform_listener.h>
+#include "tf/message_filter.h"
+#include "message_filters/subscriber.h"
 
 #include <pcl_ros/transforms.h>
 #include <pcl/ros/conversions.h>
@@ -14,8 +16,9 @@ using namespace std;
 
 ros::Publisher pub;
 tf::TransformListener *listener;
+tf::MessageFilter<sensor_msgs::PointCloud2> *tf_filter;
 
-void callback(const sensor_msgs::PointCloud2::ConstPtr& msg)
+void callback(const boost::shared_ptr<const sensor_msgs::PointCloud2>& msg)
 {
   ROS_INFO("Called callback");
   sensor_msgs::PointCloud2 pc_out;
@@ -37,7 +40,10 @@ int main(int argc, char **argv)
 
   ros::NodeHandle private_node_handle_("~");
   private_node_handle_.param("pc2_input", pc_sub_topic, string("/ptu_sweep/depth/points"));
-  ros::Subscriber sub = n.subscribe(pc_sub_topic.c_str(), 10, callback);
+  message_filters::Subscriber<sensor_msgs::PointCloud2> sub;
+  sub.subscribe(private_node_handle_, pc_sub_topic.c_str(), 10);
+  tf_filter = new tf::MessageFilter<sensor_msgs::PointCloud2>(sub, *listener, "/base_link", 10);
+  tf_filter->registerCallback(boost::bind(&callback, _1));
   private_node_handle_.param("pc2_output", pc_pub_topic, string("/transform_pc2/depth/points"));
   pub = n.advertise<sensor_msgs::PointCloud2>(pc_pub_topic.c_str(), 10);
 
