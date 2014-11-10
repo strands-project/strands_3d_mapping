90556fe7ed9b4973d791c2263e7cbb00b0535059
Rares Ambrus <raambrus@kth.se>
2014-11-10 16:25:39 +0100

    Parsing xml file taken in as a command line argument

--- MetaroomXMLParser/src/loadSingleFileMain.cpp
+++ MetaroomXMLParser/src/loadSingleFileMain.cpp
@@ -6,11 +6,20 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+    if (argc < 2)
+    {
+        cout<<"Please provide XML path as argument."<<endl;
+        return -1;
+    } else {
+
+    }
+
+
     SimpleXMLParser<PointType> parser;
 
     SimpleXMLParser<PointType>::RoomData roomData;
 
-    roomData = parser.loadRoomFromXML("SOME_PATH/room_0/room.xml");
+    roomData = parser.loadRoomFromXML(argv[1]);
 
     cout<<"Complete cloud size "<<roomData.completeRoomCloud->points.size()<<endl;
 
@@ -18,7 +27,5 @@ int main(int argc, char** argv)
     {
         cout<<"Intermediate cloud size "<<roomData.vIntermediateRoomClouds[i]->points.size()<<endl;
         cout<<"Fx: "<<roomData.vIntermediateRoomCloudCamParams[i].fx()<<" Fy: "<<roomData.vIntermediateRoomCloudCamParams[i].fy()<<endl;
-
-
     }
 }
