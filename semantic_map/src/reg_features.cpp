e2220573a43d6cf94c73211ffb64063d9b788ba3
Rares Ambrus <raambrus@kth.se>
2015-03-12 19:53:52 +0100

    Computing orb features from intermediate images making up a sweep and saving them to disk

---
+++ semantic_map/src/reg_features.cpp
@@ -0,0 +1,11 @@
+#include <semantic_map/reg_features.h>
+
+RegistrationFeatures::RegistrationFeatures(bool verbose, std::string data_filename)
+{
+
+}
+
+RegistrationFeatures::~RegistrationFeatures()
+{
+
+}
