f8a07b64b806a556d22cbde78914ad1096517c4a
Rares <raambrus@kth.se>
2015-03-20 18:11:36 +0000

    Mask extraction

--- object_manager/include/object_manager.h
+++ object_manager/include/object_manager.h
@@ -16,17 +16,11 @@
 #include "std_msgs/String.h"
 #include <tf/transform_listener.h>
 
-#include <mongodb_store/message_store.h>
-
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/image_encodings.h>
 
 // Services
-#include <semantic_map_publisher/MetaroomService.h>
 #include <object_manager/DynamicObjectsService.h>
-#include <semantic_map_publisher/ObservationService.h>
-#include <semantic_map_publisher/ObservationOctomapService.h>
-#include <semantic_map_publisher/WaypointInfoService.h>
 
 // PCL includes
 #include <pcl_ros/point_cloud.h>
@@ -38,12 +32,15 @@
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <pcl/registration/distances.h>
 #include <pcl/search/kdtree.h>
+#include <pcl/visualization/pcl_visualizer.h>
 #include "load_utilities.h"
 
 #include <geometry_msgs/Point.h>
 #include <pcl/common/centroid.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/frustum_culling.h>
 
+#include <semantic_map/room_xml_parser.h>
 
 template <class PointType>
 class ObjectManager {
@@ -67,6 +64,7 @@ public:
 
     bool dynamicObjectsServiceCallback(DynamicObjectsServiceRequest &req, DynamicObjectsServiceResponse &res);
     std::vector<ObjectManager<PointType>::ObjStruct>  loadDynamicObjectsFromObservation(std::string obs_file);
+    void returnObjectMask(std::string waypoint, std::string object_id, std::string observation_xml);
 
     ros::Publisher                                                              m_PublisherDynamicClusters;
     ros::ServiceServer                                                          m_DynamicObjectsServiceServer;
@@ -233,6 +231,8 @@ bool ObjectManager<PointType>::dynamicObjectsServiceCallback(DynamicObjectsServi
     res.object_id = id;
     res.centroids = centroids;
 
+    returnObjectMask(req.waypoint_id, "object_1",latest);
+
     return true;
 
 
@@ -289,5 +289,202 @@ std::vector<typename ObjectManager<PointType>::ObjStruct>  ObjectManager<PointTy
     return dynamicObjects;
 }
 
+template <class PointType>
+void ObjectManager<PointType>::returnObjectMask(std::string waypoint, std::string object_id, std::string observation_xml)
+{
+    auto it =  m_waypointToObjMap.find(waypoint);
+    if (it == m_waypointToObjMap.end() )
+    {
+        ROS_ERROR_STREAM("No objects loaded for waypoint "+waypoint);
+        return;
+    }
+
+    std::vector<ObjStruct> objects = m_waypointToObjMap[waypoint];
+    ObjStruct object;
+    bool found = false;
+    for (auto objectStruct : objects)
+    {
+        if (objectStruct.id == object_id)
+        {
+            found = true;
+            object = objectStruct;
+            break;
+        }
+    }
+
+    if (!found)
+    {
+        ROS_ERROR_STREAM("Cannot find object "+object_id+" at waypoint "+waypoint);
+        return;
+    }
+
+
+    // load observation and individual clouds
+    SemanticRoom<PointType> observation = SemanticRoomXMLParser<PointType>::loadRoomFromXML(observation_xml,true);
+
+    int argc = 0;
+    char** argv;
+    pcl::visualization::PCLVisualizer* p = new pcl::visualization::PCLVisualizer (argc, argv, "Metaroom consistency");
+    p->addCoordinateSystem();
+    CloudPtr object_cloud(new Cloud());
+    *object_cloud = *object.cloud;
+
+    // transform into the camera frame of ref (to compare with the individual clusters)
+    Eigen::Matrix4f roomTransform = observation.getRoomTransform();
+    if (observation.getIntermediateCloudTransforms().size() ==0)
+    {
+        ROS_ERROR_STREAM("Cannot get transform to origin.");
+        return;
+    }
+
+    tf::StampedTransform transformToOrigin = observation.getIntermediateCloudTransforms()[0];
+
+//    pcl::transformPointCloud (*object_cloud, *object_cloud, roomTransform.inverse());
+//    pcl_ros::transformPointCloud(*object_cloud, *object_cloud,transformToOrigin.inverse());
+
+
+
+
+    std::vector<tf::StampedTransform> allTransforms = observation.getIntermediateCloudTransformsRegistered();
+    std::vector<CloudPtr> allClouds = observation.getIntermediateClouds();
+    // frustrum for centroid projection
+
+    pcl::FrustumCulling<PointType> fc;
+    fc.setInputCloud (object_cloud);
+    fc.setVerticalFOV (45);
+    fc.setHorizontalFOV (15);
+    fc.setNearPlaneDistance (.5);
+    fc.setFarPlaneDistance (3.5);
+
+    int max_overlap = 0;
+    int best_index = -1;
+    Eigen::Matrix4f best_transform;
+
+    for (int j=0; j<allTransforms.size();j++)
+    {
+
+        Eigen::Matrix4f eigen_combined;
+        pcl_ros::transformAsMatrix (transformToOrigin*allTransforms[j], eigen_combined);
+
+        Eigen::Matrix4f cam2robot;
+        cam2robot << 0, 0, 1, 0, 0,-1, 0, 0,1, 0, 0, 0, 0, 0, 0, 1;
+        Eigen::Matrix4f pose_new = roomTransform * eigen_combined ;
+
+        // set pose to frustum
+        fc.setCameraPose (pose_new * cam2robot);
+        pcl::PointCloud <PointType> target;
+        fc.filter (target);
+
+//        ROS_INFO_STREAM("Overlap "<<target.points.size() <<" max overlap"<<max_overlap);
+
+        if (target.points.size() > max_overlap)
+        {
+//            ROS_INFO_STREAM("Intermediate cloud "<<j<<" contains the current dynamic cluster. No Points "<<target.points.size()<<"  total points "<<object_cloud->points.size());
+            max_overlap = target.points.size();
+            best_index = j;
+            best_transform = pose_new;
+        }
+
+    }
+
+    if (max_overlap > 0)
+    {
+        ROS_INFO_STREAM("Intermediate cloud "<<best_index<<" contains the current dynamic cluster. No Points "<<max_overlap<<"  total points "<<object_cloud->points.size());
+        CloudPtr transformedCloud(new Cloud());
+        pcl::transformPointCloud (*allClouds[best_index], *transformedCloud, best_transform);
+
+
+        // filter out the cluster from the intermediate cloud
+        CloudPtr filteredFromIntCloudCluster(new Cloud());
+        std::vector<int> nn_indices (1);
+        std::vector<float> nn_distances (1);
+        std::vector<int> src_indices;
+        pcl::SegmentDifferences<PointType> segment;
+        typename pcl::search::KdTree<PointType>::Ptr tree (new pcl::search::KdTree<PointType>);
+        tree->setInputCloud (object_cloud);
+
+          // Iterate through the source data set
+          for (int i = 0; i < static_cast<int> (transformedCloud->points.size ()); ++i)
+          {
+            if (!isFinite (transformedCloud->points[i]))
+              continue;
+            // Search for the closest point in the target data set (number of neighbors to find = 1)
+            if (!tree->nearestKSearch (transformedCloud->points[i], 1, nn_indices, nn_distances))
+            {
+              PCL_WARN ("No neighbor found for point %zu (%f %f %f)!\n", i, transformedCloud->points[i].x, transformedCloud->points[i].y, transformedCloud->points[i].z);
+              continue;
+            }
+
+            if (nn_distances[0] < 0.001)
+            {
+                src_indices.push_back (i);
+                filteredFromIntCloudCluster->push_back(transformedCloud->points[i]);
+            }
+          }
+
+
+
+
+        ROS_INFO_STREAM("Filtered cluster from int cloud "<<filteredFromIntCloudCluster->points.size()<<"  inliers "<<src_indices.size());
+
+        cv::Mat cluster_image = cv::Mat::zeros(480, 640, CV_8UC3);
+        int top_y = -1, bottom_y = 640, top_x = -1, bottom_x = 640;
+        for (int index : src_indices)
+        {
+            pcl::PointXYZRGB point = transformedCloud->points[index];
+            int y = index / cluster_image.cols;
+            int x = index % cluster_image.cols;
+            cluster_image.at<cv::Vec3b>(y, x)[0] = point.b;
+            cluster_image.at<cv::Vec3b>(y, x)[1] = point.g;
+            cluster_image.at<cv::Vec3b>(y, x)[2] = point.r;
+
+            if (y > top_y)
+                top_y = y;
+            if (x > top_x)
+                top_x = x;
+            if (y < bottom_y)
+                bottom_y = y;
+            if (x < bottom_x)
+                bottom_x = x;
+
+        }
+
+        ROS_INFO_STREAM("Image ROI "<<bottom_y<<" "<<bottom_x<<" "<<top_y - bottom_y<<" "<<top_x - bottom_x);
+
+//        if ((bottom_y < 0) || (bottom_x < 0) || (top_y - bottom_y < 0) || (top_x - bottom_x) < 0)
+//        {
+//        }
+
+        cv::Rect rec(bottom_x,bottom_y,top_x - bottom_x, top_y - bottom_y);
+        cv::Mat tmp = cluster_image(rec);
+
+
+
+        cv::imwrite("image.jpg", cluster_image);
+
+
+            cv::imshow( "Display window", cluster_image );                   // Show our image inside it.
+            cv::waitKey(0);                                          // Wait for a keystroke in the window
+
+
+            pcl::visualization::PointCloudColorHandlerCustom<PointType> cluster_handler (object_cloud, 0, 255, 0);
+            pcl::visualization::PointCloudColorHandlerCustom<PointType> int_handler (transformedCloud, 255, 255, 0);
+            p->addPointCloud(object_cloud,cluster_handler,"cluster");
+            p->addPointCloud(transformedCloud,int_handler,"int");
+            p->spin();
+            p->removeAllPointClouds();
+
+            // found a matching intermediate cloud
+//            pcl::visualization::PointCloudColorHandlerCustom<PointType> int_cloud_handler (transformedCloud, 255, 0, 0);
+//            pcl::visualization::PointCloudColorHandlerCustom<PointType> cluster_handler (c, 0, 255, 0);
+//            pcl::visualization::PointCloudColorHandlerCustom<PointType> cluster_from_int_handler (filteredFromIntCloudCluster, 0, 0, 255);
+//            p->addPointCloud (transformedCloud, int_cloud_handler, "int_cloud");
+//            p->addPointCloud (c, cluster_handler, "cluster_cloud");
+//            p->addPointCloud (filteredFromIntCloudCluster, cluster_from_int_handler, "cluster_from_int_cloud_cloud");
+//            p->spin();
+//            p->removeAllPointClouds();
+    }
+
+}
 
 #endif
