7c2a14159337af011e3c62cf51a6eef5013dc88f
Rares <raambrus@kth.se>
2016-04-19 16:32:10 +0100

    First pass of object learning fixes

--- learn_objects_action/src/learn_objects_action/machine_y3.py
+++ learn_objects_action/src/learn_objects_action/machine_y3.py
@@ -65,11 +65,11 @@ class LearnObjectActionMachineYear3(smach.StateMachine):
             self._stop_static_tf = StopSendingTransformation(debug_mode, self._debug_services)
             smach.StateMachine.add('STOP_TRANSFORM', self._stop_static_tf,
                 transitions={'error':'failed',
-                             'success':'LEARN_MODEL' })
-            smach.StateMachine.add('LEARN_MODEL', LearnObjectModel(model_path,debug_mode, self._debug_services),
-                transitions={'error':'failed',
-                             'done':'succeeded',
-                            'preempted': 'preempted' })
+                             'success':'succeeded' })
+            #smach.StateMachine.add('LEARN_MODEL', LearnObjectModel(model_path,debug_mode, self._debug_services),
+            #    transitions={'error':'failed',
+            #                 'done':'succeeded',
+            #                'preempted': 'preempted' })
 
             self.set_initial_state(["METRIC_MAP_SWEEP"], userdata=self.userdata)
             self.register_termination_cb(self.finish)
