0ad303a774d70920f00d5f4d25d7098d3e8f96ed
Nils Bore <nilsbore@gmail.com>
2016-11-21 13:45:13 +0100

    Added a new method for constructing soma_llsd scenes

--- quasimodo/quasimodo_conversions/include/quasimodo_conversions/conversions.h
+++ quasimodo/quasimodo_conversions/include/quasimodo_conversions/conversions.h
@@ -8,12 +8,21 @@
 #include <quasimodo_msgs/rgbd_frame.h>
 #include <ros/ros.h>
 
+#include <opencv2/opencv.hpp>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+
 namespace quasimodo_conversions {
 
 void model_to_soma_segment(ros::NodeHandle& n, const quasimodo_msgs::model& model, soma_llsd_msgs::Segment& segment);
 void soma_segment_to_model(ros::NodeHandle& n, const soma_llsd_msgs::Segment& segment, quasimodo_msgs::model& model);
 void soma_observation_to_frame(ros::NodeHandle& n, const soma_llsd_msgs::Observation& obs, quasimodo_msgs::rgbd_frame& frame);
 void frame_to_soma_observation(ros::NodeHandle& n, const quasimodo_msgs::rgbd_frame& frame, soma_llsd_msgs::Observation& obs);
+void raw_frames_to_soma_scene(const cv::Mat& rgb, const cv::Mat& depth,
+                              const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& cloud,
+                              const Eigen::Matrix4d& pose, const Eigen::Matrix3d& K,
+                              const std::string& waypoint, const std::string episode_id,
+                              soma_llsd_msgs::Scene& scene);
 
 } // namespace quasimodo_conversions
 
--- quasimodo/quasimodo_conversions/src/quasimodo_conversions/conversions.cpp
+++ quasimodo/quasimodo_conversions/src/quasimodo_conversions/conversions.cpp
@@ -2,7 +2,37 @@
 #include <soma_llsd/GetScene.h>
 #include <soma_llsd/InsertScene.h>
 
+#include <pcl_ros/point_cloud.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <tf_conversions/tf_eigen.h>
+#include <eigen_conversions/eigen_msg.h>
+
 namespace quasimodo_conversions {
+
+void convert_to_img_msg(const cv::Mat& cv_image, sensor_msgs::Image& ros_image)
+{
+    cv_bridge::CvImagePtr cv_pub_ptr(new cv_bridge::CvImage);
+    cv_pub_ptr->image = cv_image;
+    cv_pub_ptr->encoding = "bgr8";
+    ros_image = *cv_pub_ptr->toImageMsg();
+}
+
+void convert_to_depth_msg(const cv::Mat& cv_image, sensor_msgs::Image& ros_image)
+{
+    cv_bridge::CvImagePtr cv_pub_ptr(new cv_bridge::CvImage);
+    cv_pub_ptr->image = cv_image;
+    cv_pub_ptr->encoding = "mono16";
+    ros_image = *cv_pub_ptr->toImageMsg();
+}
+
+void convert_to_mask_msg(const cv::Mat& cv_image, sensor_msgs::Image& ros_image)
+{
+    cv_bridge::CvImagePtr cv_pub_ptr(new cv_bridge::CvImage);
+    cv_pub_ptr->image = cv_image;
+    cv_pub_ptr->encoding = "mono8";
+    ros_image = *cv_pub_ptr->toImageMsg();
+}
 
 void model_to_soma_segment(ros::NodeHandle& n, const quasimodo_msgs::model& model, soma_llsd_msgs::Segment& segment)
 {
@@ -131,6 +161,55 @@ void soma_observation_to_frame(ros::NodeHandle& n, const soma_llsd_msgs::Observa
     //frame.camera = // see above
     frame.frame_id = std::stoi(obs.id);
     frame.pose = obs.pose; // not good actually but whatevs
+}
+
+//string episode_id
+//string waypoint
+//string meta_data
+//uint32 timestamp
+
+//tf/tfMessage transform
+//sensor_msgs/PointCloud2 cloud
+//sensor_msgs/Image rgb_img
+//sensor_msgs/Image depth_img
+//sensor_msgs/CameraInfo camera_info
+//geometry_msgs/Pose robot_pose
+//---
+//bool result
+//soma_llsd_msgs/Scene response
+
+// ### response has id field! ###
+
+void raw_frames_to_soma_scene(const cv::Mat& rgb, const cv::Mat& depth,
+                              const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& cloud,
+                              const Eigen::Matrix4d& pose, const Eigen::Matrix3d& K,
+                              const std::string& waypoint, const std::string episode_id,
+                              soma_llsd_msgs::Scene& scene)
+{
+    convert_to_img_msg(rgb, scene.rgb_img);
+    convert_to_depth_msg(depth, scene.depth_img);
+    pcl::toROSMsg(*cloud, scene.cloud);
+    tf::poseEigenToMsg(Eigen::Affine3d(pose), scene.robot_pose);
+    sensor_msgs::CameraInfo info;
+    info.K.at(0) = K(0, 0);
+    info.K.at(1) = K(0, 1);
+    info.K.at(2) = K(0, 2);
+    info.K.at(3) = K(1, 0);
+    info.K.at(4) = K(1, 1);
+    info.K.at(5) = K(1, 2);
+    info.K.at(8) = K(2, 2);
+
+    info.P.at(0) = K(0, 0);
+    info.P.at(1) = K(0, 1);
+    info.P.at(2) = K(0, 2);
+    info.P.at(4) = K(1, 0);
+    info.P.at(5) = K(1, 1);
+    info.P.at(6) = K(1, 2);
+    info.P.at(10) = K(2, 2);
+    info.height = rgb.rows;
+    info.width = rgb.cols;
+    scene.waypoint = waypoint;
+    scene.episode_id = episode_id;
 }
 
 } // namespace quasimodo_conversions
