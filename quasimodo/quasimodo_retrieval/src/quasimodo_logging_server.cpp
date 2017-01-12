4c43f7bc57e4785d808c5893906f9f51b6180dc2
Nils Bore <nilsbore@gmail.com>
2017-01-12 15:52:18 +0100

    Removed unnecessary output and filled in some more soma fields to make analysis easier

--- quasimodo/quasimodo_retrieval/src/quasimodo_logging_server.cpp
+++ quasimodo/quasimodo_retrieval/src/quasimodo_logging_server.cpp
@@ -25,6 +25,22 @@ using PointT = pcl::PointXYZRGB;
 using CloudT = pcl::PointCloud<PointT>;
 using HistT = pcl::Histogram<250>;
 
+string random_string( size_t length )
+{
+    auto randchar = []() -> char
+    {
+        const char charset[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+        const size_t max_index = (sizeof(charset) - 1);
+        return charset[ rand() % max_index ];
+    };
+    string str(length,0);
+    generate_n(str.begin(), length, randchar);
+    return str;
+}
+
 class logging_server {
 public:
     ros::NodeHandle n;
@@ -42,16 +58,22 @@ public:
     {
         soma_manager::SOMAInsertObjs::Request soma_req;
 
+        string search_id = random_string(6);
+
         size_t N = res.result.retrieved_clouds.size();
         soma_req.objects.resize(N);
         for (size_t i = 0; i < N; ++i) {
-            cout << "Retrieved image paths length: " << res.result.retrieved_image_paths.size() << endl;
+            //cout << "Retrieved image paths length: " << res.result.retrieved_image_paths.size() << endl;
             boost::filesystem::path sweep_xml = boost::filesystem::path(res.result.retrieved_image_paths[i].strings[0]).parent_path() / "room.xml";
-            cout << "Sweep xml: " << sweep_xml.string() << endl;
+            //cout << "Sweep xml: " << sweep_xml.string() << endl;
             boost::posix_time::time_duration diff;
             if (sweep_xml.parent_path().parent_path().stem() == "temp") {
                 soma_req.objects[i].cloud = res.result.retrieved_clouds[i];
-                diff = boost::posix_time::time_duration(10, 10, 10); // just placeholder for now, should get real timestamp and transform from mongodb object
+                //diff = boost::posix_time::time_duration(10, 10, 10); // just placeholder for now, should get real timestamp and transform from mongodb object
+                boost::posix_time::ptime start_time = boost::posix_time::second_clock::local_time();
+                boost::posix_time::ptime time_t_epoch(boost::gregorian::date(1970,1,1));
+                diff = start_time - time_t_epoch;
+                soma_req.objects[i].type = "quasimodo_db_result";
             }
             else {
                 auto data = SimpleXMLParser<PointT>::loadRoomFromXML(sweep_xml.string(), vector<string>({"RoomIntermediateCloud"}), false, false);
@@ -62,12 +84,15 @@ public:
                 cout << "Intermediate room cloud transforms length: " << data.vIntermediateRoomCloudTransforms.size() << endl;
                 tf::transformTFToEigen(data.vIntermediateRoomCloudTransforms[0], AT);
                 pcl_ros::transformPointCloud(AT.matrix().cast<float>(), res.result.retrieved_clouds[i], soma_req.objects[i].cloud);
+                soma_req.objects[i].type = "quasimodo_metaroom_result";
             }
             //soma_req.objects[i].cloud = res.result.retrieved_clouds[i];
             cout << "Retrieved initial poses length: " << res.result.retrieved_initial_poses.size() << endl;
             soma_req.objects[i].pose = res.result.retrieved_initial_poses[i].poses[0];
             soma_req.objects[i].logtimestamp = diff.total_seconds(); //   ros::Time::now().sec;
             soma_req.objects[i].id = res.result.retrieved_image_paths[i].strings[0];
+            soma_req.objects[i].config = "quasimodo_retrieval_result";
+            soma_req.objects[i].metadata = search_id;
         }
 
         std::sort(soma_req.objects.begin(), soma_req.objects.end(), [](const soma_msgs::SOMAObject& o1, const soma_msgs::SOMAObject& o2) {
@@ -89,14 +114,14 @@ public:
         soma_manager::SOMAInsertObjs::Response soma_resp;
         try {
             if (soma_service.call(soma_req, soma_resp)) {
-                cout << "Successfully inserted queries!" << endl;
+                cout << "Quasimodo logging server: successfully inserted queries!" << endl;
             }
             else {
-                cout << "Could not connect to SOMA!" << endl;
+                cout << "Quasimodo logging server: Could not connect to SOMA!" << endl;
             }
         }
         catch (exception& e) {
-            cout << "Soma service called failed with: " << e.what() << endl;
+            cout << "Quasimodo logging server: Soma service called failed with: " << e.what() << endl;
         }
 
         /*
